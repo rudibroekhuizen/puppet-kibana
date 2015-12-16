7e3b42f476960b5ece66907aee4256b163e56973
Rudi Broekhuizen <rudi.broekhuizen@naturalis.nl>
2015-12-16 20:34:18 +0100

    Update package.pp

--- manifests/package.pp
+++ manifests/package.pp
@@ -7,14 +7,14 @@ class kibana::package {
     checksum => false,
     target   => $kibana::install_dir,
     url      => $kibana::download_url,
-    notify   => Exec['chown_kibana_directory'],
+    #notify   => Exec['chown_kibana_directory'],
   }
  
-  exec { 'chown_kibana_directory':
-    command     => "chown -R ${kibana::kibana_user}:${kibana::kibana_group} ${kibana::install_dir}/kibana",
-    path        => ['/bin','/sbin'],
-    refreshonly => true,
-    require     => $symlink_require,
-  }
+  #exec { 'chown_kibana_directory':
+  #  command     => "chown -R ${kibana::kibana_user}:${kibana::kibana_group} ${kibana::install_dir}/kibana",
+  #  path        => ['/bin','/sbin'],
+  #  refreshonly => true,
+  #  require     => $symlink_require,
+  #}
   
 }
