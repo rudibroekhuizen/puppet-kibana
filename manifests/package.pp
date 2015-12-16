06ebc864d8aa80dd4f2181449a273fd33db67b33
Rudi Broekhuizen <rudi.broekhuizen@naturalis.nl>
2015-12-16 15:05:12 +0100

    Create package.pp

---
+++ manifests/package.pp
@@ -0,0 +1,20 @@
+# == Class: kibana::package
+#
+class kibana::package {
+
+  archive { 'kibana':
+    ensure   => present,
+    checksum => false,
+    target   => $kibana::install_dir,
+    url      => $kibana::download_url,
+    notify   => Exec['chown_kibana_directory'],
+  }
+ 
+  exec { 'chown_kibana_directory':
+    command     => "chown -R ${kibana::kibana_user}:${kibana::kibana4_group} ${kibana::install_dir}/kibana-${version}",
+    path        => ['/bin','/sbin'],
+    refreshonly => true,
+    require     => $symlink_require,
+  }
+  
+}
