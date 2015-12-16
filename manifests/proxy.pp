3009d61d6371feae60e0e01d18f5edea48045fda
Rudi Broekhuizen <rudi.broekhuizen@naturalis.nl>
2015-12-16 20:23:17 +0100

    Update proxy.pp

--- manifests/proxy.pp
+++ manifests/proxy.pp
@@ -4,11 +4,25 @@ class kibana::proxy {
 
   class { 'nginx': }
 
-  nginx::resource::upstream { 'kibana': members => ['localhost:5601', ],}
+  # Create password file
+  httpauth { 'kibadmin':
+    file      => '/etc/nginx/.htpasswd',
+    password  => 'password',
+    realm     => 'realm',
+    mechanism => basic,
+    ensure    => present,
+  }
+
+  # Create proxy
+  nginx::resource::upstream { 'kibana':
+    members => ['localhost:5601', ],
+  },
+  
   nginx::resource::vhost { 'kibana':
     proxy                => 'http://localhost:5601',
     auth_basic           => 'Restricted Content',
     auth_basic_user_file => '/etc/nginx/.htpasswd'
+    require              => Httpauth['kibadmin']
   }
 
 }
