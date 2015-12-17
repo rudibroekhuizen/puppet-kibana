784d2d71df1aa55e8dc6960614be5787c3ac3f68
Rudi Broekhuizen <rudi.broekhuizen@naturalis.nl>
2015-12-17 14:41:05 +0100

    Update proxy.pp

--- manifests/proxy.pp
+++ manifests/proxy.pp
@@ -13,7 +13,7 @@ class kibana::proxy {
     notify    => Service['nginx'],
   }
 
-  # Set permissions on .htpasswd file
+  # Set correct permissions on .htpasswd file
   file { '/etc/nginx/.htpasswd':
     mode    => '0644',
     require => Httpauth['kibadmin']
