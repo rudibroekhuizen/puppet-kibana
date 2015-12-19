657c6f6de8b7bde3ce5a2d23179e7824ebd42f37
Rudi Broekhuizen <rudi.broekhuizen@naturalis.nl>
2015-12-19 08:56:50 +0100

    Update service.pp

--- manifests/service.pp
+++ manifests/service.pp
@@ -2,10 +2,16 @@
 #
 class kibana::service {
 
-  file { 'init':
+  file { 'kibana init':
     content => template('kibana/init.erb'),
     path    => '/etc/init.d/kibana',
     mode    => '0775',
+  }
+  
+  file {'kibana log directory':
+    ensure => directory,
+    path   => '/var/log/kibana',
+    before => Service['kibana']
   }
 
   service {'kibana':
